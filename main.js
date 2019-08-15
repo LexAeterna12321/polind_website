e602776653ee9571e1ca107a52fc2302dac01b3f
Lex Aeterna <lexaeterna12321@gmail.com>
2019-08-15 14:31:59 +0200

    popup added

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const cardBtns = document.querySelectorAll(".card__btn");
-
 cardBtns.forEach(btn => {
   btn.addEventListener("click", e => {
     e.target.parentNode.parentNode.children[0].classList.toggle(
       "card__front--hidden"
     );
   });
+});
+//
+const headerBtn = document.querySelector(".header__btn");
+const searchAnchor = document.getElementById("search");
+headerBtn.addEventListener("click", () => {
+  window.scrollTo({
+    behavior: "smooth",
+    top: searchAnchor.offsetTop
+  });
+});
+//
+const CityExplorePopupBtn = document.querySelector(".explore__btn");
+CityExplorePopupBtn.addEventListener("click", () => {
+  const popup = document.querySelector(".popup");
+  popup.classList.remove("popup--hidden");
+});
+//
+const popupCloseBtn = document.querySelector(".popup__btn");
+popupCloseBtn.addEventListener("click", e => {
+  console.log(e.target.parentNode.parentNode);
+  e.target.parentNode.parentNode.classList.add("popup--hidden");
 });
